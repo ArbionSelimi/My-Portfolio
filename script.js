fe7fbbfc908fbfc3a884b7961181f6e9645de8f9
ArbionSelimi <arbionselimi1@gmail.com>
2022-11-10 23:38:01 +0100

    Css, Js, and animations added

---
+++ script.js
@@ -0,0 +1,31 @@
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+function revealBackToTop() {
+  var revealButton = document.querySelector(".back-to-top");
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    revealButton.classList.add("active-back-to-top");
+  } else {
+    revealButton.classList.remove("active-back-to-top");
+  }
+}
+
+window.onscroll = function () {
+  revealBackToTop();
+};
